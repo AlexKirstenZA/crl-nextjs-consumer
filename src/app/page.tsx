0d06be1db87ca1e9534bfc8682b3cfcc23246759
Alex Kirsten <alexander.kirsten@gmail.com>
2025-09-17 10:39:57 +0100

    feat(initial-tooling): tweak hello world positioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="mx-auto min-h-screen max-w-screen-md px-6 md:px-12 py-12">
-      <h1 className="font-sans text-5xl font-bold">Hello world</h1>
+    <div className="mx-auto min-h-screen max-w-screen-md">
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="font-sans text-5xl font-bold">Hello world</h1>
+      </div>
     </div>
   );
 }
