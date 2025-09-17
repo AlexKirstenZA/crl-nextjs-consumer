bdc42d27a08e730f71f27dadc8ca3b970c653aaa
Alex Kirsten <alexander.kirsten@gmail.com>
2025-09-17 10:32:16 +0100

    feat(initial-tooling): install nextjs and configure initial styles

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['300', '400', '700', '900'],
+  variable: '--font-inter',
+  display: 'swap',
+});
+
+export const metadata: Metadata = {
+  title: "NextJS consumer technical exercise",
+  description: "Technical exercise for CR Senior Drupal / NextJS developer role.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="mx-auto min-h-screen max-w-screen-md px-6 md:px-12 py-12">
+      <h1 className="font-sans text-5xl font-bold">Hello world</h1>
+    </div>
+  );
+}
